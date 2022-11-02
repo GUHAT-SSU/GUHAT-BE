5c3ffb02c0b59a12361400788e4d0d5cd5eb97a0
Hyomk <qet557@gmail.com>
2022-11-02 21:56:00 +0900

    add : project setting

---
+++ controllers/authController.js
@@ -0,0 +1,6 @@
+module.exports = {
+    login: async function (req, res) {
+        console.log("login API Success");
+        res.send("success good!");
+    },
+};
--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require("express");
-const app = express();
-
+const cors = require("cors");
+const cheerio = require("cheerio");
+const axios = require("axios");
+const puppeteer = require("puppeteer");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = 8001;
+const app = express();
+
+const authRouter = require("./routes/auth");
+
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json()); // json 파싱
 
 app.set("port", process.env.PORT || PORT);
 
-app.get("/test", (req, res) => {
-    res.send("Hello from App Engine!");
+app.use("/auth", authRouter);
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
 });
 
 app.listen(app.get("port"), () => {
