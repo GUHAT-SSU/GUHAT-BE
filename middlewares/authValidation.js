aa72335f784ec9acbade039e9a220c37cde4af08
coin6442@gmail.com <coin6442@gmail.com>
2022-12-06 21:12:29 +0900

    fix: 인증 절차 로직 임시 비활성화

--- middlewares/authValidation.js
+++ middlewares/authValidation.js
@@ -3,31 +3,42 @@ const { verify, refreshVerify } = require("../utils/jwt-util");
 const jwt = require("jsonwebtoken");
 
 const authChecker = async (req, res, next) => {
-    if (req.headers.authorization) {
-        const token = req.headers.authorization.split("Bearer ")[1];
-
-        const decoded = jwt.decode(token);
-        const user = await User.findOne({ where: { id: decoded.id } });
-        if (!user) {
-            return res.status(401).send({
-                ok: false,
-                message: "Can not find user",
-            });
-        }
-
-        const refreshResult = refreshVerify(user.dataValues.token, decoded.id);
-        if (refreshResult.ok === false) {
-            return res.status(401).send({
-                ok: false,
-                message: "Auth Error from authChecker",
-            });
-        } else {
-            req.userId = decoded.id;
-            next();
-        }
-    } else {
-        res.status(401).json({ error: "Auth Error from authChecker" });
+    if (req.headers.authorization){
+        console.log("이게 문제");
+        req.userId = '20221776';
+        next();
+    }else{
+        console.log("이게 문제2");
+        req.userId = '20221776';
+        next();
     }
+   
+    
+//     if (req.headers.authorization) {
+//         const token = req.headers.authorization.split("Bearer ")[1];
+
+//         const decoded = jwt.decode(token);
+//         const user = await User.findOne({ where: { id: decoded.id } });
+//         if (!user) {
+//             return res.status(401).send({
+//                 ok: false,
+//                 message: "Can not find user",
+//             });
+//         }
+
+//         const refreshResult = refreshVerify(user.dataValues.token, decoded.id);
+//         if (refreshResult.ok === false) {
+//             return res.status(401).send({
+//                 ok: false,
+//                 message: "Auth Error from authChecker",
+//             });
+//         } else {
+//             req.userId = decoded.id;
+//             next();
+//         }
+//     } else {
+//         res.status(401).json({ error: "Auth Error from authChecker" });
+//     }
 };
 
 module.exports = { authChecker };
