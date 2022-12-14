9f329962ed3b4a4cd9914d02ce8317371838e147
Hyomk <qet557@gmail.com>
2022-12-14 20:27:56 +0900

    fix : 라우팅 오류 & end point 변경

--- routes/posting.js
+++ routes/posting.js
@@ -6,15 +6,13 @@ const router = express.Router();
 
 console.log("conneceted router posting");
 router.post("/lecture", authChecker, postController.createPost); // (check) 구인글 작성
-router.post("/lecture/apply", authChecker, postController.apply);  // (check) 구인글 작성
+router.post("/lecture/apply", authChecker, postController.apply); // (check) 구인글 작성
 
-router.get("/lecture/all", authChecker, postController.getAllPost); // 구인글 리스트 조회
-// router.get("/lecture/:postId", authChecker, postController.getPosting); // 구인글 상세 조회
+router.get("/lecture/:postId", authChecker, postController.getPosting); // 구인글 상세 조회
+router.get("/lectures", authChecker, postController.getAllPost); // 구인글 리스트 조회
+
 // router.get("/lecture/member", authChecker, postController.getAllAppliers); // 작성한 구인글 지원자 리스트 조회
 // router.get("/lecture/apply", authChecker, postController.getMyApplyPosting); // 내가 지원한 구인글 리스트 조회
-
 // router.patch("/lecture/member", authChecker, postController.updateApplyStatus); // 구인글 지원상태 변경
-
-
 
 module.exports = router;
