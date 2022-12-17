490fd1f28de4fc3c839b0b8607e8d3e00341c2d4
coin6442@gmail.com <coin6442@gmail.com>
2022-12-18 07:23:03 +0900

    chore: post는 post끼리 묶기...

--- routes/posting.js
+++ routes/posting.js
@@ -5,13 +5,14 @@ const postController = require("../controllers/postController");
 const router = express.Router();
 
 console.log("conneceted router posting");
-router.post("/lecture", authChecker, postController.createPost); // (check) 구인글 작성
-router.post("/lecture/apply", authChecker, postController.apply); // (check) 구인글 작성
+router.post("/lecture", authChecker, postController.createPost); // 구인글 작성
+router.post("/lecture/apply", authChecker, postController.apply); // 구인글 지원
+router.post("/lecture/member", authChecker, postController.updateApplyStatus); // 구인글 지원상태 변경(지원 받기)
 
 router.get("/lecture/:postId", authChecker, postController.getPosting); // 구인글 상세 조회
 router.get("/lectures", authChecker, postController.getAllPost); // 구인글 리스트 조회
 //router.get("/lecture/member", authChecker, postController.getAllAppliers); // 작성한 구인글 지원자 리스트 조회
 // router.get("/lecture/apply", authChecker, postController.getMyApplyPosting); // 내가 지원한 구인글 리스트 조회
-router.post("/lecture/member", authChecker, postController.updateApplyStatus); // 구인글 지원상태 변경
+
 
 module.exports = router;
